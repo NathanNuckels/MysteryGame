56d1af8aa5c24fb8097c857b9879d1a0924ad509
NathanNuckels <nathannuckels@gmail.com>
2021-12-09 22:15:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+print("Them by Alan")
+while True:
+	print("Commands: \"new\" \"load\" \"about\" \"exit\"")
+	cmd=input("><>")
+	if cmd=="load":
+		pass
+	elif cmd=="about":
+		print("A face made of Unicode characters")
+		print("wants something... but what?")
+	elif cmd=="exit":
+		sys.exit()
+	elif cmd=="new":
+		break
+	else:
+		print("Command not found")
+
